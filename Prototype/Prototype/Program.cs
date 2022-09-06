caf2a1b867fffa3b94fe480860aaddf58a2ad9ab
AmandaPAlfaro <amanda.perez.captiva@outlook.com>
2022-09-06 18:36:30 -0400

    Add Prototype, Singleton

---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Animal oAnimal = new Animal() {Nombre = "Oveja", Patas = 4 };
+            Animal oAnimalClonado = oAnimal.Clone() as Animal;
+            oAnimalClonado.Patas = 5;
+            Console.WriteLine(oAnimal.Patas);
+        }
+    }
+}
