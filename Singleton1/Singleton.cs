d38376f2866ad8dd9e266138fe0680cea89e67d6
AmandaPAlfaro <amanda.perez.captiva@outlook.com>
2022-09-06 18:30:45 -0400

    Singleton design pattern

---
+++ Singleton1/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton1
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        public string mensaje = "";
+
+        protected Singleton() 
+        {
+            mensaje = "Hola Mundo";
+        }
+
+        public static Singleton Instance 
+        {
+            get 
+            {
+                if (Instance == null)
+                    instance = new Singleton { };
+                return instance;
+            }
+        }
+    }
+}
