7e71a8b87c4648fda5376f247ab7043d381b3447
amandaperezalfaro <aperez.alfaro15@gmail.com>
2022-09-07 14:03:04 -0400

    Prototype_deep

---
+++ Prototype_deep/Prototype/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Animal oAnimal = new Animal() {Nombre = "Oveja", Patas = 4 };
+            oAnimal.Rasgos = new Detalles();
+            oAnimal.Rasgos.Color = "Blanca";
+            oAnimal.Rasgos.Raza = "Oveja";
+
+            Animal oAnimalClonado = oAnimal.Clone() as Animal;
+            oAnimalClonado.Rasgos.Color = "Negro";
+            oAnimalClonado.Nombre = "Oveja Negra";
+
+            Console.WriteLine("Animal Original: " + oAnimal.Rasgos.Color);
+            Console.WriteLine("Animal clonado: " + oAnimalClonado.Rasgos.Color);
+            Console.WriteLine("Animal Original: " + oAnimal.Nombre);
+            Console.WriteLine("Animal clonado: " + oAnimalClonado.Nombre);
+        }
+    }
+}
