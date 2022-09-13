30de9c94c2a59d002d85f3363f1d9ae17fce6100
amandaperezalfaro <aperez.alfaro15@gmail.com>
2022-09-13 12:50:53 -0400

    add Component

---
+++ Composite/Composite/Ingrediente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Ingrediente : Component
+    {
+        public int Cantidad { get; set; }
+        public string Unidad { get; set; }
+        public Ingrediente(string Nombre, decimal Costo, int cantidad, string unidad) : base(Nombre, Costo)
+        {
+            Cantidad = cantidad;
+            Unidad = unidad;
+        }
+    }
+}
---
+++ Composite/Composite/PastelComposite.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class PastelComposite : Component
+    {
+        private List<Component> ingredientes = new List<Component>();
+
+        public decimal CostoTotal 
+        {
+            get 
+            {
+                decimal costo = 0;
+                foreach (var oElement in ingredientes) 
+                {
+                    if (oElement.GetType().Name == "PastelComposite")
+                        costo += ((PastelComposite)oElement).CostoTotal;
+                    else
+                        costo += oElement.Costo;
+                }
+                return costo;
+            }
+        }
+
+        public void Add(Component oElement) 
+        {
+            ingredientes.Add(oElement);
+        }
+
+        public void Remove(Component oElement) 
+        {
+            ingredientes.Remove(oElement);
+        }
+
+        public PastelComposite(string Nombre, decimal Costo=0) : base(Nombre, Costo)
+        {
+
+        }
+    }
+}
